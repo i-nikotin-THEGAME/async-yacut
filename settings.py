fdfb6b3a4946b1cf3be4dc866d5a0f6968695973
i-nikotin-THEGAME <i_nikotin@thegame.design>
2026-06-09 00:27:59 +0300

    fix

--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ class Config(object):
     """Основная конфигурация приложения"""
 
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DB", "sqlite:///db.sqlite3")
+    # Оставлено DATABASE_URI, т.к. в тестах именно эту переменную просит
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Максимальный размер файла 16MB
     DISK_TOKEN = os.getenv("DISK_TOKEN", "")
